5972d018210f7b41e86995e467a45f30a8a50556
Ilizarov1 <1224722105@qq.com>
2022-08-17 16:08:20 +0800

    feat:  支持spa

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,41 @@
-import type { NextPage } from 'next';
-import Header from '../components/common/Header';
+import type { NextPage } from 'next'
+import Header from '../components/common/Header'
 
-import useSWR from 'swr';
+import useSWR from 'swr'
 
-const fetcher = (url: URL) => fetch(url).then((r) => r.json());
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+
+const fetcher = (url: URL) => fetch(url).then((r) => r.json())
 
 const Home: NextPage = () => {
-	const { data, error } = useSWR('./api/hello', fetcher);
+	const { data, error } = useSWR('./api/hello', fetcher)
 
-	if (error) return <div>failed to load</div>;
-	if (!data) return <div>loading...</div>;
+	if (error) return <div>failed to load</div>
+	if (!data) return <div>loading...</div>
 
 	return (
 		<div>
 			<Header />
-		</div>
-	);
-};
 
-export default Home;
+			<Router>
+				<div>
+					<ul>
+						<li>
+							<Link to="/">INDEX</Link>
+						</li>
+						<li>
+							<Link to="/about">ABOUT</Link>
+						</li>
+					</ul>
+
+					<Routes>
+						<Route path='/' element={(<h2>INDEX</h2>)}/>
+						<Route path='/about' element={(<h2>ABOUT</h2>)}/>
+					</Routes>
+				</div>
+			</Router>
+		</div>
+	)
+}
+
+export default Home
