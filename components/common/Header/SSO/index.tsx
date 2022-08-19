918b244e5a53f576c7e6ca8da6a9066a91cdfe2f
Ilizarov1 <1224722105@qq.com>
2022-08-19 16:20:10 +0800

    feat: mui

--- components/common/Header/SSO/index.tsx
+++ components/common/Header/SSO/index.tsx
@@ -1,17 +1,24 @@
 import Image from 'next/image';
-
-import headIcon from '../../../../public/head-icon.png';
+import ArrowDropDown from '@mui/icons-material/ArrowDropDownOutlined';
+import headIcon from '../../../../public/WechatIMG6.jpeg';
 
 import styles from './SSO.module.scss';
 
 export default function SSO() {
   return (
     <div className={styles.sso}>
-      <div className={styles.image}>
-        <Image src={headIcon} alt='' />
-      </div>
+      <Image
+        src={headIcon}
+        alt=''
+        className={styles.image}
+        height={24}
+        width={24}
+      />
       <div className={styles.username}>
         <span>Zhao</span>
+      </div>
+      <div className={styles.icon}>
+        <ArrowDropDown htmlColor='#232323' />
       </div>
     </div>
   );
